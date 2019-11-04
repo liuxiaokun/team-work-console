358e56a72a01698a313d0ee2da51963a7ede4828
liuxiaokun <liuxiaokun@baiyixinkeji.com>
2019-11-04 16:43:38 +0800

    格式化日期

--- src/view/function/list.vue
+++ src/view/function/list.vue
@@ -63,187 +63,199 @@
 </template>
 
 <script>
-import * as api from '@/api/function'
-import * as projectApi from '@/api/project'
-import moment from 'moment'
-import { ChartPie, ChartBar } from '_c/charts'
+  import * as api from '@/api/function'
+  import * as projectApi from '@/api/project'
+  import moment from 'moment'
+  import { ChartPie, ChartBar } from '_c/charts'
 
-export default {
-  name: 'list',
-  components: {
-    ChartPie,
-    ChartBar
-  },
-  data () {
-    return {
-      pieData: [
-        { value: 335, name: '开发' },
-        { value: 310, name: '上线' },
-        { value: 135, name: '回退' },
-        { value: 234, name: '测试' }
-      ],
+  export default {
+    name: 'list',
+    components: {
+      ChartPie,
+      ChartBar
+    },
+    data () {
+      return {
+        pieData: [
+          { value: 335, name: '开发' },
+          { value: 310, name: '上线' },
+          { value: 135, name: '回退' },
+          { value: 234, name: '测试' }
+        ],
 
-      barData: {
-        Mon: 22,
-        Tue: 33,
-        Wed: 22,
-        Thu: 25,
-        Fri: 43,
-        Sat: 27,
-        Sun: 65
-      },
-
-      filters: {
-        name: '',
-        projectId: ''
-      },
-      projects: [],
-      pc: {
-        pageIndex: 1,
-        pageSize: 20,
-        total: 0
-      },
-      columns: [
-        {
-          title: '#',
-          type: 'index',
-          align: 'center',
-          width: 50
+        barData: {
+          Mon: 22,
+          Tue: 33,
+          Wed: 22,
+          Thu: 25,
+          Fri: 43,
+          Sat: 27,
+          Sun: 65
         },
-        {
-          title: '所属项目',
-          key: 'projectName',
-          align: 'center',
-          width: 220
+
+        filters: {
+          name: '',
+          projectId: ''
         },
-        {
-          title: '名字',
-          render: (h, params) => {
-            return h('a', {
-              on: {
-                click: () => {
-                  this.$router.push({
-                    path: '/function/view',
-                    query: { id: params.row.id }
-                  })
+        projects: [],
+        pc: {
+          pageIndex: 1,
+          pageSize: 20,
+          total: 0
+        },
+        columns: [
+          {
+            title: '#',
+            type: 'index',
+            align: 'center',
+            width: 50
+          },
+          {
+            title: '所属项目',
+            key: 'projectName',
+            align: 'center',
+            width: 130
+          },
+          {
+            title: '名字',
+            render: (h, params) => {
+              return h('a', {
+                on: {
+                  click: () => {
+                    this.$router.push({
+                      path: '/function/view',
+                      query: { id: params.row.id }
+                    })
+                  }
                 }
-              }
-            }, params.row.name)
+              }, params.row.name)
+            }
+          },
+          {
+            title: '开发开始',
+            key: 'devStartTime',
+            align: 'center',
+            width: 150,
+            render: (h, params) => {
+              return h('span', {}, moment(params.row.devStartTime).format('YYYY-MM-DD hh:mm:ss'))
+            }
+          },
+          {
+            title: '开发结束',
+            key: 'devDeadline',
+            align: 'center',
+            width: 150,
+            render: (h, params) => {
+              return h('span', {}, moment(params.row.devDeadline).format('YYYY-MM-DD hh:mm:ss'))
+            }
+          },
+          {
+            title: '测试结束',
+            key: 'testDeadline',
+            align: 'center',
+            width: 150,
+            render: (h, params) => {
+              return h('span', {}, moment(params.row.testDeadline).format('YYYY-MM-DD hh:mm:ss'))
+            }
+          },
+          {
+            title: '上线',
+            key: 'deadline',
+            align: 'center',
+            width: 150,
+            render: (h, params) => {
+              return h('span', {}, moment(params.row.deadline).format('YYYY-MM-DD hh:mm:ss'))
+            }
+          },
+          {
+            title: '状态',
+            key: 'currentStateName',
+            width: 120,
+            render: (h, params) => {
+              let color = params.row.currentStateName === '上线完成' ? 'success' : 'warning'
+              return h('Tag', {
+                props: {
+                  'color': color
+                }
+              }, params.row.currentStateName)
+            }
+          },
+          {
+            title: '备注',
+            key: 'remark'
+          },
+          {
+            title: '时间消耗',
+            width: 300,
+            render: (h, params) => {
+              return h('i-progress', {
+                props: {
+                  percent: params.row.timeCostPercent,
+                  'stroke-width': 22,
+                  'status': 'success',
+                  'text-inside': true,
+                  'hide-info': true
+                }
+              })
+            }
           }
-        },
-        {
-          title: '开发开始',
-          key: 'devStartTime',
-          align: 'center',
-          width: 200
-        },
-        {
-          title: '开发结束',
-          key: 'devDeadline',
-          align: 'center',
-          width: 200
-        },
-        {
-          title: '测试结束',
-          key: 'testDeadline',
-          align: 'center',
-          width: 200
-        },
-        {
-          title: '上线',
-          key: 'deadline',
-          align: 'center',
-          width: 200
-        },
-        {
-          title: '状态',
-          key: 'currentStateName',
-          width: 120,
-          render: (h, params) => {
-            let color = params.row.currentStateName === '上线完成' ? 'success' : 'warning'
-            return h('Tag', {
-              props: {
-                'color': color
-              }
-            }, params.row.currentStateName)
-          }
-        },
-        {
-          title: '备注',
-          key: 'remark'
-        },
-        {
-          title: '时间消耗',
-          width: 300,
-          render: (h, params) => {
-            return h('i-progress', {
-              props: {
-                percent: params.row.timeCostPercent,
-                'stroke-width': 22,
-                'status': 'success',
-                'text-inside': true,
-                'hide-info': true
-              }
-            })
-          }
-        }
-      ],
-      data: []
-    }
-  },
-
-  created () {
-    this.loadProject()
-    this.load()
-  },
-
-  watch: {
-    'pc.pageIndex': function (newValue, oldValue) {
-      if (newValue !== oldValue) {
-        this.load()
+        ],
+        data: []
       }
     },
-    'pc.pageSize': function (newValue, oldValue) {
-      if (newValue !== oldValue) {
-        this.load()
-      }
-    }
-  },
-  methods: {
-    currentDate () {
-      return moment.format('YYYY-MM-DD hh:mm:ss')
+
+    created () {
+      this.loadProject()
+      this.load()
     },
-    loadProject () {
-      projectApi.getProjects({ p: 1, s: 2000 }).then((res) => {
-        if (res.data.success) {
-          this.projects = res.data.data
+
+    watch: {
+      'pc.pageIndex': function (newValue, oldValue) {
+        if (newValue !== oldValue) {
+          this.load()
         }
-      })
-    },
-    load () {
-      let paging = {
-        p: this.pc.pageIndex,
-        s: this.pc.pageSize
-      }
-
-      let params = Object.assign({}, paging, this.filters)
-      api.getFunctions(params).then((res) => {
-        if (res.data.success) {
-          this.data = res.data.data
-          this.pc = res.data.pc
+      },
+      'pc.pageSize': function (newValue, oldValue) {
+        if (newValue !== oldValue) {
+          this.load()
         }
-      })
+      }
     },
+    methods: {
+      currentDate () {
+        return moment.format('YYYY-MM-DD hh:mm:ss')
+      },
+      loadProject () {
+        projectApi.getProjects({ p: 1, s: 2000 }).then((res) => {
+          if (res.data.success) {
+            this.projects = res.data.data
+          }
+        })
+      },
+      load () {
+        let paging = {
+          p: this.pc.pageIndex,
+          s: this.pc.pageSize
+        }
 
-    changePageIndex (page) {
-      this.pc.pageIndex = page || 1
-    },
+        let params = Object.assign({}, paging, this.filters)
+        api.getFunctions(params).then((res) => {
+          if (res.data.success) {
+            this.data = res.data.data
+            this.pc = res.data.pc
+          }
+        })
+      },
 
-    changePageSize (num) {
-      this.pc.pageSize = parseInt(num)
+      changePageIndex (page) {
+        this.pc.pageIndex = page || 1
+      },
+
+      changePageSize (num) {
+        this.pc.pageSize = parseInt(num)
+      }
     }
   }
-}
 </script>
 
 <style scoped>
